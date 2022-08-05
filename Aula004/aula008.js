f5d8820f340917270e0f5c82d738fc2a43651361
Castrotech <107278965+Castrose@users.noreply.github.com>
2022-08-05 17:40:46 -0300

    criei exercícios novos

---
+++ Aula004/aula008.js
@@ -0,0 +1,10 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+    console.log(`Use sempre sinto de segurança!`)
+}
+
+if (vel <= 59) {
+    console.log(`Parabéns, você não foi multado! Continue andando!`)
+}
